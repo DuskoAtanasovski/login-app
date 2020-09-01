644a06644483943739d7452ceffffc177e6e1d98
DuskoAtanasovski <atanasovski95@yahoo.com>
2020-09-01 21:02:35 +0200

    Add existing file

--- src/main/java/com/login/web/UserRegistrationController.java
+++ src/main/java/com/login/web/UserRegistrationController.java
@@ -1,6 +1,7 @@
 package com.login.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -17,11 +18,16 @@ public class UserRegistrationController {
 		super();
 		this.userService = userService;
 	}
-	
+
+	@GetMapping
+	public String showRegistrationForm() {
+		return "registration";
+	}
+
 	public String registerUserAccount(@ModelAttribute("user") UserRegistrationDto registrationDto) {
 		userService.save(registrationDto);
 		return "redirect:/registration?success";
-		
+
 	}
-	
+
 }
